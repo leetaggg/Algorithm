1736f739dd41220a995d487a42ca47f3e03e8e88
leetaggg <100212241+leetaggg@users.noreply.github.com>
2023-08-02 14:15:13 +0900

    [Silver III] Title: N과 M (1), Time: 2264 ms, Memory: 63256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
-	static boolean[] visited;
-	static int[] arr;
-	static StringBuilder sb = new StringBuilder();
-
+	static int n;
+	static int m;
+	
+	static int[] numbers;
+	static boolean[] isSelected;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+//		StringBuilder sb = new StringBuilder();
+		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		visited = new boolean[n + 1];
-		arr = new int[m];
-		permutation(0);
-		System.out.println(sb.toString());
+		
+		numbers = new int[m];
+		isSelected = new boolean[n+1];
+		
+		answer(0);
+
 	}
 
-	public static void permutation(int cnt) {
-		if (cnt == m) {
-			for (int num : arr){
-				sb.append(num).append(" ");
+	/**
+	 * @param start 시작값
+	 * @param m index 담는 값
+	 */
+	public static void answer(int cnt) {
+		
+		if(cnt == m) {
+			for (int i : numbers) {
+				System.out.print(i + " ");
 			}
-			sb.append("\n");
+			System.out.println();
 			return;
 		}
+		
 		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) {
-				arr[cnt] = i;
-				visited[i] = true;
-				permutation(cnt + 1);
-				visited[i] = false;
-			}
+			if (isSelected[i])
+				continue;
+			numbers[cnt] = i;
+			isSelected[i] = true;
+			
+			answer(cnt+1);
+			isSelected[i] = false;
 		}
+		
 	}
+
 }
