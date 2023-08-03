ca813e987d4befe6b5d7ea0e0aa3f38c9c090796
leetaggg <100212241+leetaggg@users.noreply.github.com>
2023-08-03 16:37:04 +0900

    [Silver IV] Title: 카드2, Time: 140 ms, Memory: 43032 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			q.add(i);
+		}
+		while(q.size() > 1) {
+			q.remove();
+			q.add(q.poll());
+		}
+		System.out.println(q.peek());
+	}
+}
