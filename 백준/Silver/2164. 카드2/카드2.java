58807479312558b5d01e26ca2fb1a9c31bffb4e4
leetaggg <100212241+leetaggg@users.noreply.github.com>
2023-08-04 15:47:21 +0900

    [Silver IV] Title: 카드2, Time: 240 ms, Memory: 36848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,18 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new LinkedList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		Deque<Integer> q = new ArrayDeque<Integer>();
 		for (int i = 1; i <= n; i++) {
-			q.add(i);
+			q.offer(i);
 		}
 		while(q.size() > 1) {
-			q.remove();
-			q.add(q.poll());
+			q.poll();
+			int tmp = q.peek();
+			q.poll();
+			q.offer(tmp);
 		}
 		System.out.println(q.peek());
+		
 	}
+
 }
