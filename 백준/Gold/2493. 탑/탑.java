19073857a001c0266ac56e95fd30a9665c00b55a
leetaggg <100212241+leetaggg@users.noreply.github.com>
2023-08-07 16:58:18 +0900

    [Gold V] Title: 탑, Time: 928 ms, Memory: 191892 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,43 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n + 1];
+		Stack<Integer> stack = new Stack<Integer>(); // 탑의 인덱스
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-
-		int n = Integer.parseInt(st.nextToken());
-		Stack<int[]> s = new Stack<>();
-		int[] arr = new int[n];
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			int value = Integer.parseInt(st.nextToken());
-			
-			//top이 0이거나 들어온 값이  스택에 있는 값보다 클 경우에
-			while(!s.isEmpty()) {
-				if(s.peek()[1] >= value) {
-					sb.append(s.peek()[0]+1).append(" ");
-					break;
-				}
-				s.pop();
+		for (int i = 1; i <= n; i++) {
+			// 탑의 값을 입력 받아서 arr 저장
+			arr[i] = Integer.parseInt(st.nextToken());
+			// 스택에 나보다 작은 높이의 탑은 꺼내서 버리기(pop()) : 언제까지 반복?
+			// 		공백 스택일 때까지  or 스택에 나보다 크거나 같은 탑이 남아 있을때까지
+			while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+				stack.pop(); // 꺼내 버리기
 			}
-			if (s.isEmpty()) {
-				sb.append(0).append(" ");
+			if(stack.isEmpty()) { // 공백 스택
+				sb.append("0 ");
+			} else {
+				sb.append(stack.peek() + " ");
 			}
-				
-			s.push(new int[] {i, value});
-//			System.out.println(Arrays.toString(arr));
+			stack.push(i);
 		}
-		//구현
-		System.out.println(sb);
-
+		System.out.println(sb.toString());
 	}
-
 }
