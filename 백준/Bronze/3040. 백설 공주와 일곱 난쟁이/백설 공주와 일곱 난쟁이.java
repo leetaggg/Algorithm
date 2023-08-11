4aea4772bb7f3ec15b03d6db59dce885dcbc4d99
leetaggg <100212241+leetaggg@users.noreply.github.com>
2023-08-11 11:38:45 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 80 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,57 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
-public class Main{
-
-	private static int[] arr;
-	private static boolean[] isSelected;
-
+public class Main {
+	static int[] arr, nums;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		//백설공주 - 의자 7, 접시 7, 나이프 7
-		//일곱난쟁이의 모자에 쓰여있는 합이 100
-		//부분집합의 합 이용??
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
 		arr = new int[9];
-		isSelected = new boolean[9];
-		
-		for (int i = 0; i < 9; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i] = Integer.parseInt(st.nextToken());
+		nums = new int[7];
+		for(int i = 0; i < 9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		
-		Subset(0);
-		System.out.println(sb);
-		
-		
+		combination(0, 0);
+		System.out.println(sb.toString());
 	}
-	public static void Subset(int cnt) {
-		if(cnt == 9) {
+	
+	
+	public static void combination(int cnt, int start) {
+		if(cnt == 7) {
 			int sum = 0;
-			int count = 0;
-			for (int i = 0; i < 9; i++) {
-				if(isSelected[i] ) {
-					sum += arr[i];
-					count++;
-				}
+			for(int i : nums) {
+				sum += arr[i];
 			}
-			if(sum == 100 && count == 7) {
-				for (int i = 0; i < 9; i++) {
-					if(isSelected[i]) {
-						sb.append(arr[i]).append("\n");
-					}
+			if(sum == 100) {
+				for(int i : nums) {
+					sb.append(arr[i]).append("\n");
 				}
 			}
 			return;
 		}
-		isSelected[cnt] = true;
-		Subset(cnt+1);
-		isSelected[cnt] = false;
-		Subset(cnt+1);
-				
+		
+		for(int i = start; i < 9; i++) {
+			nums[cnt] = i;
+			combination(cnt + 1, i + 1);
+		}
 	}
 }
