15d83c5cf38be5da8d3340201f79b7ab94b130e6
leetaggg <100212241+leetaggg@users.noreply.github.com>
2023-08-13 22:32:10 +0900

    [Silver III] Title: 이전 순열, Time: 124 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.java
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -1,75 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        if (!nextPermutation(nums, N)) {
+            System.out.println(-1);
+        } else {
+            for (int number : nums) {
+                sb.append(number).append(" ");
+            }
+            System.out.println(sb.toString());
+        }
+    }
 
-	private static Integer[] arr;
+    public static boolean nextPermutation(int[] arr, int n) {
+        int top = arr.length - 1;
+        while (top > 0 && arr[top - 1] <= arr[top])
+            --top;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		// 입력
-		int n = Integer.parseInt(st.nextToken());
-		arr = new Integer[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        if (top == 0) {
+            reverse(arr, 0, arr.length - 1); // 배열 전체를 뒤집어서 초기 순열로 돌아감
+            return false;
+        }
+        int swapIdx = arr.length - 1;
 
-		// 내림차순인 순간 찾기
-		int nx = -1;
-		for (int i = n - 1; i > 0; i--) {
-			if (arr[i - 1] > arr[i]) {
-				nx = i-1;
-				break;
-			}
-		} // end for
-		if (nx == -1) {
-			sb.append(-1).append(" ");
-			System.out.println(sb.toString());
+        while (arr[top - 1] <= arr[swapIdx]) {
+            --swapIdx;
+        }
+        swap(arr, top - 1, swapIdx);
 
-			return;
-		}
-		int ny = 0;
-		int max = Integer.MIN_VALUE;
-		for (int i = n - 1; i > nx; i--) {
-			// arr[nx] 보다 작으면서 가장 큰값
-			if (arr[i] < arr[nx] && max < Math.max(arr[i], max)) {
-				max = Math.max(arr[i], max);
-				ny = i;
-			}
+        swapIdx = arr.length - 1;
+        while (top < swapIdx) {
+            swap(arr, top, swapIdx);
+            top++;
+            swapIdx--;
+        }
+        return true;
+    }
 
-		}
-		swap(nx, ny);
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
-		Comparator<Integer> cp = new Comparator<Integer>() {
-
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				// TODO Auto-generated method stub
-				return o2.compareTo(o1);
-			}
-		};
-		
-		Arrays.sort(arr, nx+1, n, cp);
-		
-		for (Integer num : arr) {
-			sb.append(num).append(" ");
-		}
-		
-		System.out.println(sb.toString());
-	}
-
-	private static void swap(int x, int y) {
-		int tmp = arr[x];
-		arr[x] = arr[y];
-		arr[y] = tmp;
-
-	}
-
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
 }
