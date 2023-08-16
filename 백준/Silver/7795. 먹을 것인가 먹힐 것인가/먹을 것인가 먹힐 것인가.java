f0df4bbc542a541b4abbbe8f0f2811dbe78cc250
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-17 08:16:26 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1088 ms, Memory: 41888 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,49 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int t = Integer.parseInt(st.nextToken());//testcase
-		
-		for (int test_case = 0; test_case < t; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken()); //a의 수
-			int m = Integer.parseInt(st.nextToken()); //b의 수
-			
-			Integer[] a = new Integer[n];
-			Integer[] b = new Integer[m];
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n; i++) {
-				a[i] = Integer.parseInt(st.nextToken());
-			}
-			Arrays.sort(a, Collections.reverseOrder());
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < m; i++) {
-				b[i] = Integer.parseInt(st.nextToken());
-			}
-			Arrays.sort(b, Collections.reverseOrder());
-			
-			int s = 0;
-			int answer = 0;
-			for (int i = 0; i < m; i++) {
-				while(s < n && b[i] < a[s]) s++;
-				answer += s;
-//				System.out.println("i값:"+i+"지점:"+s);
-			}
-			
-			sb.append(answer).append("\n");
-		}//end test_case
-		System.out.println(sb);
-		
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int test_case = 0; test_case < t; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int[] arrA = new int[n];
+            int[] arrB = new int[m];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arrA[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                arrB[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arrA);
+            Arrays.sort(arrB);
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++){
+                    if(arrA[i] <= arrB[j]) break;
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
 }
