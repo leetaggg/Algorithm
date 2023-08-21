c090c8d72d6aabd28cde412332ec306e1159f9a1
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-21 13:38:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 17416 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,32 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[] arr = new int[100001];
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		bfs(n, k);
-		System.out.println(arr[k]);
-	}
-	
-	public static void bfs(int n, int k) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		if(n == k) return;
-		q.add(n);
-		Loop:
-		while(!q.isEmpty()) {
-			int x = q.poll();
-			int[] dx = {x + 1, x - 1, x * 2};
-			for(int i : dx) {
-				if(i >= 0 && i < 100001 && arr[i] == 0) {
-					arr[i] = arr[x] + 1;
-					if(i == k) break Loop;
-					q.add(i);
+		
+		int[] dist = {-1, 1, 2}; //좌우, 점프
+		boolean[] isVisited = new boolean[100_001];
+		
+		Queue<int[]> queue = new ArrayDeque<>(); 
+		queue.offer(new int[] {n, 0}); // 시작점 넣기, 시간
+		int min = Integer.MAX_VALUE;
+		int[] current;
+		
+		while(!queue.isEmpty()) {
+			current = queue.poll();
+			isVisited[current[0]] = true;
+			
+			int next;
+			if(current[0] == k) {
+				min = Math.min(min, current[1]);
+				break;
+			}
+			
+			for (int i = 0; i < dist.length; i++) {
+				if(i == 2) {
+					next = current[0]*dist[i];
+				}else {
+					next = current[0] + dist[i];
+				} //다음값
+				
+				//범위체크
+				if(next < 0 || next > 100_000 || isVisited[next]) {
+					continue;
 				}
+				
+				isVisited[next] = true;
+				queue.offer(new int[] {next, current[1]+1});
+				
 			}
 		}
-	} 
+		System.out.println(min);
+		
+	}
+
 }
