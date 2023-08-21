da8a70d0005d1eba8ac864749b85deb54afdfd69
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-21 13:44:03 +0900

    [Silver II] Title: 결혼식, Time: 208 ms, Memory: 18856 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,39 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int[] count;
-    static List<List<Integer>> list = new ArrayList<>();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
-        for(int i = 0; i < m; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int to = Integer.parseInt(st.nextToken());
-            int from = Integer.parseInt(st.nextToken());
-            list.get(to).add(from);
-            list.get(from).add(to);
-        }
 
-        count = new int[n + 1];
-        dfs(1, 0);
-        int sum = 0;
-        for (int i = 2; i <= n ; i++) {
-            if(count[i] != 0) sum++;
-        }
-        System.out.println(sum);
-    }
+	private static ArrayList<Integer>[] Sang;
+	private static boolean[] isVisited;
 
-    public static void dfs(int start, int depth){
-        if(depth == 2){
-            return;
-        }
-        for(int next : list.get(start)){
-            count[next]++;
-            dfs(next, depth + 1);
-        }
-    }
+	static int cnt;
+	
+	public static void main(String[] args) throws IOException {
+		
+		//그래프 시작 정점은 1
+		//next.next의 data까지
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		Sang = new ArrayList[n+1] ;
+		for (int i = 1; i <= n; i++) {
+			Sang[i] = new ArrayList<Integer>();
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			Sang[s].add(e);
+			Sang[e].add(s);
+		}
+		
+		isVisited = new boolean[n+1];
+		isVisited[1] = true;
+		dfs(1, 0);
+
+		for (boolean is : isVisited) {
+			if(is) {cnt++;}
+		}
+		System.out.println(cnt-1);
+	}
+
+	public static void dfs(int v, int depth) {
+		if(depth == 2) {
+			return;
+		}
+		//인접노드 검색
+
+		for (int i : Sang[v]) {
+			isVisited[i] = true;
+			dfs(i, depth+1);
+					
+		}
+	}
 }
