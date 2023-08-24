173676db20754a83a439e4341c2775c0f6799cb1
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-24 17:55:21 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 580 ms, Memory: 129908 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,58 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        
+    static List<Integer>[] graph;
+    static int n;
+    static int[] indegree, rank;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] adjMatrix = new int[n+1][n+1];
-        int[] in_ver = new int[n+1];
-        int[] result = new int[n+1];
-        
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            adjMatrix[a][b] = adjMatrix[b][a] = 1; //a,b 친구
-            in_ver[b]++; //진입 차수 올리기
-        }
-        
-        Queue<Integer> queue = new ArrayDeque<Integer>();
+        graph = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
-            if(in_ver[i] == 0) {//진입 차수가 0이면 큐에 넣기
-                queue.offer(i); //시작
-                result[i] = 1;
+            graph[i] = new ArrayList<>();
+        }
+        indegree = new int[n + 1];
+        rank = new int[n + 1];
+        Arrays.fill(rank, 1);
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from].add(to);
+            indegree[to]++;
+        }
+        topological();
+        for (int i = 1; i <= n; i++) {
+            sb.append(rank[i]).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+    public static void topological(){
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            if(indegree[i] == 0){
+                q.offer(i);
             }
         }
-        
-        while(!queue.isEmpty()) {
-            int current = queue.poll();
-//            System.out.println("c: "+current);
-            for (int i = 1; i <= n; i++) {
-                if(adjMatrix[current][i] > 0) {
-                	in_ver[i]--;
-                	
-                	if(in_ver[i] == 0) {
-                		queue.offer(i);
-                		result[i] = result[current] + 1;
-                	}//진입차수가 0이면 큐에 넣기
+        while (!q.isEmpty()){
+            int node = q.poll();
+            for (int next : graph[node]) {
+                if(--indegree[next] == 0){
+                    rank[next] = rank[node] + 1;
+                    q.offer(next);
                 }
             }
-            
-        }//while 끝
-        
-        for (int i = 1; i <= n; i++) {
-            System.out.print(result[i] +" ");
         }
     }
-
 }
