73d3891288b07751abc1765936bc41e67d597b4c
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-25 15:09:24 +0900

    [Gold IV] Title: 최단경로, Time: 1100 ms, Memory: 119696 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,58 +1,92 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	static List<List<Node>> graph;
-	static int[] dist;
-	static final int INF = Integer.MAX_VALUE;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(br.readLine());
-		graph = new ArrayList<List<Node>>();
-		dist = new int[v + 1];
-		for(int i = 0; i <= v; i++) {
-			graph.add(new ArrayList<>());
+
+	static class Node implements Comparable<Node>{
+		int num, weight;
+		
+		public Node(int num, int weight) {
+			this.num = num;
+			this.weight = weight;
 		}
-		for(int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			graph.get(Integer.parseInt(st.nextToken())).add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
-		Arrays.fill(dist, INF);
-		dijkstra(k);
-		for(int i = 1; i <= v; i++) {
-			sb.append(dist[i] == INF ? "INF" : dist[i]).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
-	static void dijkstra(int start) {
-		Queue<Node> pq = new PriorityQueue<Node>((o1, o2) -> Integer.compare(o1.weight, o2.weight));
-		pq.offer(new Node(start, 0));
-		dist[start] = 0;
-		while(!pq.isEmpty()) {
-			Node node = pq.poll();
-			int nodeIdx = node.idx;
-			int nodeWeight = node.weight;
-			for(Node next : graph.get(nodeIdx)) {
-				if(dist[next.idx] > dist[nodeIdx] + next.weight) {
-					dist[next.idx] = dist[nodeIdx] + next.weight;
-					pq.add(new Node(next.idx, dist[next.idx]));
-				}
-			}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.weight, o.weight);
 		}
 	}
 	
-	static class Node{
-		int idx;
-		int weight;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		Node(int idx, int weight) {
-			super();
-			this.idx = idx;
-			this.weight = weight;
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		
+		//그래프 연결
+		List<Node>[] graph = new ArrayList[V+1];
+		for (int i = 0; i < graph.length; i++) {
+			graph[i] = new ArrayList<>();
 		}
+
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			graph[u].add(new Node(v, w)); //그래프 연결
+		}
+		
+		boolean[] isVisited = new boolean[V+1];
+		//큐생성
+		Queue<Node> queue = new PriorityQueue<>();
+		queue.offer(new Node(k, 0)); //시작점 넣기
+		
+		int min = Integer.MAX_VALUE;
+		int[] answer = new int[V+1];
+		
+		final int INF = Integer.MAX_VALUE;
+		Arrays.fill(answer, INF); //정답 배열 초기화
+		answer[k] = 0;
+		
+		while(!queue.isEmpty()) { //시작점 꺼내면서 인접 리스트 보기
+			
+			Node current = queue.poll();//현재 꺼낸점
+			if(isVisited[current.num]) continue;
+			
+			isVisited[current.num] = true; //꺼냈으면 방문 체크 - 끝남 
+			
+			if(answer[current.num] > current.weight) {
+				answer[current.num] = current.weight; //answer에 가중치 저장
+			}
+			
+			//인접한 정점 검색
+			for (int i = 0; i < graph[current.num].size(); i++) { // 현재 정점에 인접한 리스트 보기
+				Node now = graph[current.num].get(i); //인접 리스트
+				queue.offer(new Node(now.num, answer[current.num]+ now.weight));//정점이랑 가중치 넣기
+			}
+		}
+		
+		for (int i = 1; i < answer.length; i++) {
+			if(answer[i] == Integer.MAX_VALUE)
+				System.out.println("INF");
+			else
+				System.out.println(answer[i]);
+		}
+		
 	}
+
 }
