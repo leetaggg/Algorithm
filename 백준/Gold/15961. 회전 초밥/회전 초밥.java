7786cff69ac9a9d812ba36cf054808bc9bd3b1a5
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-25 09:50:58 +0900

    [Gold IV] Title: 회전 초밥, Time: 508 ms, Memory: 168844 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,38 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    static int n, d, k, c, maxCnt;
-    static int[] sushi;
-    static int[] arr;
-    public static void main(String[] args) throws IOException{
+public class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken()); // 회전 초밥 접시 수
-        d = Integer.parseInt(st.nextToken()); // 초밥 가짓 수
-        k = Integer.parseInt(st.nextToken()); // 연속해서 먹을 수 있는 수
-        c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
-        sushi = new int[n + k];
-        arr = new int[d + 1];
-        for (int i = 0; i < n; i++) {
-            sushi[i] = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[] tmp = new int[d + 1];
+        int[] arr = new int[N + k];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        for(int i = 0; i < k; i++) {
-            sushi[n + i] = sushi[i];
-        }
-        Set<Integer> set = new HashSet<>();
-        arr[c]++;
-        set.add(c);
+
         for (int i = 0; i < k; i++) {
-            arr[sushi[i]]++;
-            set.add(sushi[i]);
+            arr[i + N] = arr[i];
+            tmp[arr[i]]++;
         }
-        int cnt = set.size();
-        for(int i = 1; i <= n; i++) {
-            if(--arr[sushi[i - 1]] == 0) cnt--;
-            if(++arr[sushi[i + k - 1]] == 1) cnt++;
+        tmp[c]++;
+
+        int cnt = 0;
+        for (int i = 0; i < tmp.length; i++) {
+            if(tmp[i] >= 1) cnt++;
+        }
+        int maxCnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if (++tmp[arr[i + k - 1]] == 1) {
+                cnt++;
+            }
+            if (--tmp[arr[i - 1]] == 0) {
+                cnt--;
+            }
+
             maxCnt = Math.max(maxCnt, cnt);
         }
+
         System.out.println(maxCnt);
     }
 }
