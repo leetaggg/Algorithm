5422cf8fafe3943d14dd8518e8f5c52d54c83805
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-08-30 14:26:41 +0900

    [Silver II] Title: 이동하기, Time: 464 ms, Memory: 76780 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,35 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		int[][] maze = new int[n+1][m+1];
-		int[][] gift = new int[n+1][m+1]; //memo
-		
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 1; j <= m; j++) {
-				maze[i][j] = Integer.parseInt(st.nextToken());
-			}
-		} // 입력 완료
-		
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) {
-				gift[i][j] = Math.max(gift[i-1][j], Math.max(gift[i-1][j-1], gift[i][j-1]))
-						+ maze[i][j];
-			}
-		}
-		System.out.println(gift[n][m]);
-		
-	}//메인 끝
+        int[][] map = new int[n][m];
+        int[][] dp = new int[n][m];
 
-}// 클래스 끝
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][0] = map[0][0];
+
+        for (int i = 1; i < m; i++) {
+            dp[0][i] = map[0][i] + dp[0][i - 1];
+        }
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = map[i][0] + dp[i - 1][0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = Math.max(map[i][j] + dp[i][j - 1], Math.max(map[i][j] + dp[i - 1][j], map[i][j] + dp[i - 1][j - 1]));
+            }
+        }
+
+        System.out.println(dp[n - 1][m - 1]);
+
+    }
+}
