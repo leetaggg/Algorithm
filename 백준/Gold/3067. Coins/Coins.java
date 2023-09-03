faf3b68ffcc468dfc1317afa5b8da2f3efa35285
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-09-03 22:31:46 +0900

    [Gold V] Title: Coins, Time: 88 ms, Memory: 11780 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[20];
+        int[] dp = new int[10001];
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 0; testCase < T; testCase++) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int m = Integer.parseInt(br.readLine());
+
+            Arrays.fill(dp, 0);
+
+            dp[0] = 1;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = arr[i]; j <= m; j++) {
+                    dp[j] += dp[j - arr[i]];
+                }
+            }
+
+            sb.append(dp[m]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
