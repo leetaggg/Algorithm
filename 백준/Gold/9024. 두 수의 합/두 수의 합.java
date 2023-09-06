d12d3e249b598879ee55345ad161c25b9ff956d7
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-09-06 09:33:38 +0900

    [Gold V] Title: 두 수의 합, Time: 1384 ms, Memory: 177952 KB -BaekjoonHub

--- 백준/Gold/9024. 두 수의 합/두 수의 합.java
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         int[] arr = new int[1000001];
 
         for (int testCase = 1; testCase <= T; testCase++) {
+            System.gc();
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
@@ -39,7 +41,8 @@ public class Main {
                     cnt++;
                 }
             }
-            System.out.println(cnt);
+            sb.append(cnt).append("\n");
         }
+        System.out.println(sb);
     }
 }
