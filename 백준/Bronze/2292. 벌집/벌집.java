d0f07b63575749c3b6167db0ce52e05ce0d094fd
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-09-07 23:53:48 +0900

    [Bronze II] Title: 벌집, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 1;
+		int range = 2; 
+ 
+		if (n == 1) {
+			System.out.print(1);
+		}else {
+			while (range <= n) { 
+				range = range + (6 * cnt); 
+				cnt++;
+			}
+			System.out.print(cnt);
+		}
+	}
+}
