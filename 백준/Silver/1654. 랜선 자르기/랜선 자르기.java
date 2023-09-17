c95fce2b389e8b41b92b898645e4b0cfb14cf2d0
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-09-17 15:33:28 +0900

    [Silver II] Title: 랜선 자르기, Time: 128 ms, Memory: 15372 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,52 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int[] lan;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int K = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		
-		lan = new int[K];
-		for (int i = 0; i < K; i++) {
-			lan[i] = Integer.parseInt(br.readLine());
-		}
-		// 정렬 
-		Arrays.sort(lan);
-		//이분탐색 - 시작 인덱스, 끝 인덱스
-		long start = 1;
-		long end = lan[K-1]; //1~최댓값
-		
-		long res = 0; //정답
-		while(start <= end) {
-			long target = (start + end)/2; //찾을 값
-//			System.out.println(answer(target));
-			if(answer(target) >= N) {
-				res = target; //저장
-				start = target+1;
-			}
-			else {
-				end = target-1;
-			}
-			
-		}//이분 탐색
-		
-		System.out.println(res);
-		
-	}
-	//계산
-	
-	static public long answer(long target) {
-		long sum = 0;
-		//lan을 다 돌면서
-		for (int i = 0; i < lan.length; i++) {
-			sum += lan[i] / target;
-		}
-		return sum;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] lan = new int[k];
+        long end = 0;
+        for (int i = 0; i < k; i++) {
+            lan[i] = Integer.parseInt(br.readLine());
+            if(end < lan[i]){
+                end = lan[i];
+            }
+        }
+
+        long start = 1;
+        long mid;
+        while (start <= end){
+            mid = (start + end) / 2;
+            long cnt = 0;
+            for (int i : lan) {
+                cnt += (i / mid);
+            }
+
+            if(cnt >= n){
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(end);
+    }
 }
