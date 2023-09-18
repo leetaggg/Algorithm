910ba1dc56081fad9f066f3eef982730e7430032
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-09-18 13:29:28 +0900

    [Gold V] Title: 내려가기, Time: 464 ms, Memory: 57020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -2,35 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[n][3];
-        int[][][] dp = new int[n][3][2];
+	static class Min_Max{
+		int min;
+		int max;
+		
+		public Min_Max(int min, int max) {
+			this.min = min;
+			this.max = max;
+		}
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		@Override
+		public String toString() {
+			return "[min=" + min + ", max=" + max + "]";
+		}
+		
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][3];
+		Min_Max[][] dp = new Min_Max[n][3];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if(i == 0) {
+					dp[i][j] = new Min_Max(arr[i][j], arr[i][j]); 
+				}
+			}
+		} //입력 끝
+//		System.out.println(Arrays.deepToString(dp));
+		for (int i = 1; i < n; i++) {
+			
+			for (int j = 0; j < 3; j++) {
+				if(j == 0) {
+					int min  = arr[i][j] + Math.min(dp[i-1][j].min, dp[i-1][j+1].min);
+					int max  = arr[i][j] + Math.max(dp[i-1][j].max, dp[i-1][j+1].max);
+					dp[i][j] = new Min_Max(min, max);
+				}else if(j == 1) {
+					int min  = arr[i][j] + Math.min(dp[i-1][j-1].min, Math.min(dp[i-1][j].min, dp[i-1][j+1].min));
+					int max  = arr[i][j] + Math.max(dp[i-1][j-1].max, Math.max(dp[i-1][j].max, dp[i-1][j+1].max));
+					dp[i][j] = new Min_Max(min, max);
+				}else if(j == 2) {
+					int min  = arr[i][j] + Math.min(dp[i-1][j].min, dp[i-1][j-1].min);
+					int max  = arr[i][j] + Math.max(dp[i-1][j].max, dp[i-1][j-1].max);
+					dp[i][j] = new Min_Max(min, max);
+				}
+				
+			}
+			
+		} //저장 끝
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < 3; i++) {
+			if(min > Math.min(min, dp[n-1][i].min)) {
+				min = dp[n-1][i].min;
+			}
+			if(max < Math.max(max, dp[n-1][i].max)) {
+				max = dp[n-1][i].max;
+			}
+		}
+		System.out.printf("%d %d", max, min);
+		
+	}
 
-        dp[0][0][0] = dp[0][0][1] = map[0][0];
-        dp[0][1][0] = dp[0][1][1] = map[0][1];
-        dp[0][2][0] = dp[0][2][1] = map[0][2];
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0][0] = Math.max(dp[i - 1][0][0], dp[i - 1][1][0]) + map[i][0];
-            dp[i][0][1] = Math.min(dp[i - 1][0][1], dp[i - 1][1][1]) + map[i][0];
-
-            dp[i][1][0] = Math.max(dp[i - 1][0][0], Math.max(dp[i - 1][1][0], dp[i - 1][2][0])) + map[i][1];
-            dp[i][1][1] = Math.min(dp[i - 1][0][1], Math.min(dp[i - 1][1][1], dp[i - 1][2][1])) + map[i][1];
-
-            dp[i][2][0] = Math.max(dp[i - 1][1][0], dp[i - 1][2][0]) + map[i][2];
-            dp[i][2][1] = Math.min(dp[i - 1][1][1], dp[i - 1][2][1]) + map[i][2];
-        }
-
-        System.out.println(Math.max(dp[n - 1][0][0], Math.max(dp[n - 1][1][0], dp[n - 1][2][0])) + " " + Math.min(dp[n - 1][0][1], Math.min(dp[n - 1][1][1], dp[n- 1][2][1])));
-    }
 }
