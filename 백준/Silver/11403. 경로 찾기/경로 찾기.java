977942f27093883478fc64124a990397b95c140e
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-09-29 22:00:36 +0900

    [Silver I] Title: 경로 찾기, Time: 132 ms, Memory: 13424 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] graph = new int[n][n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int weight = Integer.parseInt(st.nextToken());
+                if(weight == 1){
+                    graph[i][j] = 1;
+                }else{
+                    graph[i][j] = 1000;
+                }
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(graph[i][j] > graph[i][k] + graph[k][j]){
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(graph[i][j] == 1000){
+                    sb.append(0).append(" ");
+                }else{
+                    sb.append(1).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
