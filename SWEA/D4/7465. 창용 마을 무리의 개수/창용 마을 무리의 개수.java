2c685052c19541c87f853606557d5d6f2192e78c
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-10 17:55:28 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 132 ms, Memory: 25,848 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -3,68 +3,60 @@ import java.util.*;
 
 public class Solution {
 
-	private static boolean[] visited;
-	private static int n;
-	private static int m;
-	static List<Integer>[] town;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int t = Integer.parseInt(br.readLine());
-		
-		for (int testCase = 1; testCase <= t; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			
-			n = Integer.parseInt(st.nextToken());//창용마을에 사는 사람의 수
-			m = Integer.parseInt(st.nextToken());//서로를 알고 있는 사람의 관계 수
-			visited = new boolean[n+1]; // 방문 배열
-			int answer = 0;
-			town = new ArrayList[n+1];
-			
-			for (int i = 0; i < n+1; i++) {
-				town[i] = new ArrayList<Integer>();
-			}// 초기화
-			
-			for (int i = 0; i < m; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
-				town[a].add(b);
-				town[b].add(a);
-			} // 입력 끝
-			
-			//시작점은 1 부터
-			for (int i = 1; i <= n; i++) { //1번 부터 n번까지
-				if(!visited[i]) {
-					bfs(i);
-					answer++;
-				}
-			}
-			
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
-		}// end testCase
-		System.out.println(sb.toString());
-	}//end main
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    static int count;
 
-	private static void bfs(int start) {
-		Queue<Integer> queue = new ArrayDeque<Integer>();
-		queue.offer(start);
-		visited[start] = true;
-		
-		while(!queue.isEmpty()) {
-			int current = queue.poll();
-			
-			for (int i = 0; i < town[current].size(); i++) {
-				int target = town[current].get(i);
-				if(!visited[target]) {
-					queue.offer(target);
-					visited[target] = true;
-				}
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= t; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-}//end class
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            graph = new ArrayList<>();
+
+            for (int i = 0; i <= n; i++) {
+                graph.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int to = Integer.parseInt(st.nextToken());
+                int from = Integer.parseInt(st.nextToken());
+
+                graph.get(to).add(from);
+                graph.get(from).add(to);
+            }
+            visited = new boolean[n + 1];
+            count = 0;
+            for (int i = 1; i <= n; i++) {
+                if(!visited[i]){
+                    bfs(i);
+                }
+            }
+
+            sb.append("#").append(testCase).append(" ").append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for (int i : graph.get(node)) {
+                if(!visited[i]){
+                    visited[i] = true;
+                    q.offer(i);
+                }
+            }
+        }
+        count++;
+    }
+}
