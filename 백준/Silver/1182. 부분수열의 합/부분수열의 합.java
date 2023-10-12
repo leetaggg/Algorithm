1e7ebbc5262bbd7cc0b444d6f13e86af0f35a7eb
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-12 20:25:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 88 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,54 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	private static int n;
-	private static int s;
-	private static int[] input;
-	private static boolean[] isSelected;
-	static int answer;
+    static int n, s, cnt;
+    static int[] num;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		n = Integer.parseInt(st.nextToken());
-		s = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
 
-		input = new int[n];
-		isSelected = new boolean[n];
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < n; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
+        num = new int[n];
 
-		answer = 0;
-		PowerSet(0, 0);
+        st = new StringTokenizer(br.readLine());
 
-		if (s == 0) {
-			answer--;
-		}
-		System.out.println(answer);
-	}
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
 
-	private static void PowerSet(int cnt, int sum) {
+        cnt = 0;
 
-		// 기저조건
-		if (cnt == n) {
-			if (sum == s) {
-				answer++;
-			}
-			return;
-		}
-		isSelected[cnt] = true;
-		PowerSet(cnt + 1, sum + input[cnt]);
-		isSelected[cnt] = false;
-		PowerSet(cnt + 1, sum);
+        getSubsequence(0, 0, 0);
+        System.out.println(cnt);
+    }
 
-	}
+    static void getSubsequence(int sum, int depth, int count){
+        if(depth == n){
+            if(count > 0 && sum == s){
+                cnt++;
+            }
+            return;
+        }
 
+        getSubsequence(sum + num[depth], depth + 1, count + 1);
+        getSubsequence(sum, depth + 1, count);
+    }
 }
