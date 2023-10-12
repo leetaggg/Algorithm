953bbbe6253013af0164cb7abcd11568e597d95f
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-12 20:18:52 +0900

    [Gold V] Title: ZOAC, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Gold/16719. ZOAC/ZOAC.java
+++ 백준/Gold/16719. ZOAC/ZOAC.java
@@ -2,50 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static String line;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int size;
 
-	private static String input;
-	static StringBuilder sb = new StringBuilder();
-	static int size;
-	static boolean[] isVisited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		input = br.readLine();
-		size = input.length();
-		isVisited = new boolean[size];
-		recu(0, size);
-		
-		System.out.println(sb.toString());
-	}//main 끝
-	
-	public static void recu(int left, int right) {
-		
-		//기저조건
-		if(left == right) {
-			return;
-		}
-		
-		//찾고 - 오/왼 (recu(): 범위 지정해줘야해) 으로 가야함 
-		int min = 100;
-		int index = 0;
-		for (int i = left; i < right; i++) {
-			if(min > input.charAt(i)) {
-				min = input.charAt(i);
-				index = i;
-			}
-		}
-		isVisited[index] = true;
-		//답출력
-		for (int i = 0; i < isVisited.length; i++) {
-			if(isVisited[i]) {
-				sb.append(input.charAt(i));
-			}
-		}
-		sb.append("\n");
-		recu(index+1, right);
-		recu(left, index);
-		
-	}	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        line = br.readLine();
+        size = line.length();
+        visited = new boolean[size];
 
-}//class끝
+        getLine(0, size - 1);
+
+        System.out.println(sb);
+    }
+
+    static void getLine(int left, int right){
+        if(left > right) return;
+
+        int mid = left;
+
+        for (int i = left; i <= right; i++) {
+            if(line.charAt(mid) > line.charAt(i)) mid = i;
+        }
+        
+        visited[mid]= true;
+        for (int i = 0; i < size; i++) {
+            if(visited[i]) sb.append(line.charAt(i));
+        }
+        sb.append("\n");
+
+        getLine(mid + 1, right);
+        getLine(left, mid - 1);
+    }
+}
