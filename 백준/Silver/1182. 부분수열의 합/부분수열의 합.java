9ea8d183c796f2ac4e03d5af7c16d7240c43433e
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-12 10:39:30 +0900

    [Silver II] Title: 부분수열의 합, Time: 164 ms, Memory: 15488 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+
+	private static int n;
+	private static int s;
+	private static int[] input;
+	private static boolean[] isSelected;
+	static int answer;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		
+		input = new int[n];
+		isSelected = new boolean[n];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		answer = 0;
+		PowerSet(0,0);
+		System.out.println(answer);
+	}
+
+	private static void PowerSet(int cnt, int sum) {
+		
+		//기저조건
+		if(cnt == n) {
+			if(sum == s) {
+				int res = 0;
+				for (int i = 0; i < isSelected.length; i++) {
+					if(!isSelected[i]) {
+						res++;
+					}
+				}
+				if(res != isSelected.length) {
+					answer++;
+				}
+			}
+			return;
+		}
+		isSelected[cnt] = true;
+		PowerSet(cnt+1, sum+input[cnt]);
+		isSelected[cnt] = false;
+		PowerSet(cnt+1, sum);
+		
+	}
+
+}
