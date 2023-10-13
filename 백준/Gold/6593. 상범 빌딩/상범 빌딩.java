56bcee06b218086c8cc38ef3784e333ac3c98b51
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-13 11:13:06 +0900

    [Gold V] Title: 상범 빌딩, Time: 180 ms, Memory: 16700 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -1,83 +1,127 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[][][] arr;
-    static int L, R, C;
-    static int[] end;
-    static int[][] dir = {{0, 0, 1}, {0, 0, -1}, {0, 1, 0}, {0, -1, 0}, {-1, 0, 0}, {1, 0, 0}};
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            L = Integer.parseInt(st.nextToken());
-            R = Integer.parseInt(st.nextToken());
-            C = Integer.parseInt(st.nextToken());
-            
-            if(L == 0 && R == 0 && C == 0) break;
-            
-            arr = new int[L][R][C];
-            int[] start = new int[3]; 
-            end = new int[3];
-            for(int i = 0; i < L; i++) {
-                for(int j = 0; j < R; j++) {
-                    char[] chArr = br.readLine().toCharArray();
-                    for(int k = 0; k < C; k++) {
-                        char token = chArr[k];
-                        if(token == 'S') {
-                            start[0] = i;
-                            start[1] = j;
-                            start[2] = k;
-                            arr[i][j][k] = -1;
-                        }else if(token == 'E') {
-                            end[0] = i;
-                            end[1] = j;
-                            end[2] = k;
-                            arr[i][j][k] = -1;
-                        } else if(token == '.') {
-                            arr[i][j][k] = -1;
-                        }
-                    }
-                }
-                br.readLine();
-            }
-            bfs(start);
-            if(arr[end[0]][end[1]][end[2]] == 0) {
-            	sb.append("Trapped!");
-            }else {
-            	sb.append("Escaped in ").append(arr[end[0]][end[1]][end[2]]).append(" minute(s).");
-            }
-        	System.out.println(sb.toString());
-        	sb.setLength(0);
-        }
-    }
-    
-    public static void bfs(int[] start) {
-        Queue<int[]> q = new LinkedList<int[]>();
-        q.add(start);
-        Loop:
-        while(!q.isEmpty()) {
-            int[] nextXYZ = q.poll();
-            int nextX = nextXYZ[0];
-            int nextY = nextXYZ[1];
-            int nextZ = nextXYZ[2];
-            for(int[] dXYZ : dir) {
-                int dx = nextX + dXYZ[0];
-                int dy = nextY + dXYZ[1];
-                int dz = nextZ + dXYZ[2];
-                if(0 <= dx && dx < L && 0 <= dy && dy < R && 0 <= dz && dz < C && arr[dx][dy][dz] == -1) {
-                	if(dx == end[0] && dy == end[1] && dz == end[2]) {
-                    	arr[dx][dy][dz] = arr[nextX][nextY][nextZ] + 1;
-                        break Loop;
-                    }
-                    arr[dx][dy][dz] = arr[nextX][nextY][nextZ] + 1;
-                    q.add(new int[] {dx, dy, dz});
-                }
-            }
-            arr[nextX][nextY][nextZ] = 0;
-        }
-        arr[end[0]][end[1]][end[2]] += 1;
-    } 
-}
+
+	static class Location {
+		int l;
+		int r;
+		int c;
+		int cnt;
+		
+		public Location(int l, int r, int c, int cnt) {
+			this.l = l;
+			this.r = r;
+			this.c = c;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public String toString() {
+			return "Location [l=" + l + ", r=" + r + ", c=" + c + ", cnt=" + cnt + "]";
+		}
+		
+	}
+	static int[][] dist = {{0, 1, 0}, {0, -1, 0}, {1, 0, 0}, {-1, 0, 0}, {0, 0, -1}, {0, 0, 1}}; //동서남북상하 -> <- 
+	static int answer;
+	private static char[][][] map;
+	private static int l;
+	private static int r;
+	private static int c;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			//입력
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			l = Integer.parseInt(st.nextToken()); //빌딩의 층수
+			r = Integer.parseInt(st.nextToken()); //한층 행
+			c = Integer.parseInt(st.nextToken());//한층 열
+			
+			if(l == 0 && r == 0 && c == 0)
+				break;
+			
+			answer = Integer.MAX_VALUE; //초기화
+			map = new char[l][r][c];
+			int sk = 0;
+			int si = 0;
+			int sj = 0;
+			for (int k = 0; k < l; k++) { //층
+				for (int i = 0; i < r; i++) { //행
+					String input = br.readLine();
+					for (int j = 0; j < c; j++) { //열
+						map[k][i][j] = input.charAt(j);
+						if(map[k][i][j] == 'S') { //시작점 만나면 bfs
+							sk = k;
+							si = i;
+							sj = j;
+						}
+					}
+				}
+				br.readLine();
+			}
+			
+			bfs(sk, si, sj); 
+
+			//탈출 불가능이라면
+			if(answer == Integer.MAX_VALUE) {
+				sb.append("Trapped!").append("\n");
+			}else {
+				//탈출 가능하다면
+				sb.append("Escaped in ").append(answer).append(" minute(s).").append("\n");
+			}
+			
+			
+		}//입력 끝
+		System.out.println(sb.toString());
+	}//main 끝
+	
+	public static void bfs(int k, int i, int j) { //층, 행, 열
+		Queue<Location> queue = new ArrayDeque<Location>();
+		queue.offer(new Location(k, i, j, 0));
+		map[k][i][j] = 'V'; //방문처리
+		
+		while(!queue.isEmpty()) {
+			Location current = queue.poll();
+//			map[current.l][current.r][current.c] = 'V'; //방문처리
+					
+			for (int d = 0; d < dist.length; d++) {
+				int nx = current.r + dist[d][0];
+				int ny = current.c + dist[d][1];
+				int nl = current.l + dist[d][2];
+				
+				//범위, 방문 체크
+				if(nl < 0 || nl >= l || nx < 0 || nx >= r || ny < 0 || ny >= c || map[nl][nx][ny] == 'V') {
+					continue;
+				}
+				
+				//출구 e
+				if(map[nl][nx][ny] == 'E') {
+					answer = current.cnt + 1;
+					return;
+				}
+				
+				//지나갈 수 없는 칸 #
+				if(map[nl][nx][ny] == '#') {
+					map[nl][nx][ny] = 'V';
+					continue;
+				}
+				//비어있는 칸 . : 동서남북상하 가능
+				//층끼리 . 이면 층 이동 가능
+				if(map[nl][nx][ny] == '.') {
+					queue.offer(new Location(nl, nx, ny, current.cnt+1));
+					map[nl][nx][ny] = 'V';
+				}
+			}
+			
+		} //큐 끝
+		
+	}//end bfs
+	
+}//class 끝
+
