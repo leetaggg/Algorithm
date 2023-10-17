463521bc7994b818d1019e3d420619eff4348488
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-17 16:25:52 +0900

    [Silver II] Title: 예산, Time: 160 ms, Memory: 15112 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,41 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] money = new int[n];
 
-        int left = 1, mid = 0, right = 0;
+	private static int[] arr;
+	private static int budget;
 
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            money[i] = Integer.parseInt(st.nextToken());
-            if(right < money[i]) right = money[i];
-        }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		budget = Integer.parseInt(br.readLine());
+		
+		int left = 1;
+		int right = max+1;
+		
+		while(left+1 < right) {
+			int target = (left + right)/2;
+			
+			if(!check(target)) {
+				right = target;
+			}else {
+				left = target;
+			}
+		}
+		
+		System.out.println(left);
+	}// end main
 
-        int max = Integer.parseInt(br.readLine());
-
-        while(left <= right){
-            mid = (left + right) / 2;
-            sum = 0;
-
-            for (int i : money) {
-                if(i > mid){
-                    sum += mid;
-                }else{
-                    sum += i;
-                }
-            }
-
-            if(sum <= max){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
-        }
-        System.out.println(right);
-    }
-}
+	private static boolean check(int target) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if(target <= arr[i]) {
+				sum += target;
+			}else {
+				sum += arr[i];
+			}
+		}
+		
+		if(sum > budget) {
+			return false;
+		}
+		return true;
+	}
+}//end class
