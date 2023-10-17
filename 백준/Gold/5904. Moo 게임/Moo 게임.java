4934d4bd5b7b6c3c00b36fdcca1dd91a17f04439
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-17 16:33:29 +0900

    [Gold V] Title: Moo 게임, Time: 76 ms, Memory: 11456 KB -BaekjoonHub

--- 백준/Gold/5904. Moo 게임/Moo 게임.java
+++ 백준/Gold/5904. Moo 게임/Moo 게임.java
@@ -1,56 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int length = 3;
+        int k = 0;
+        while(length < n){
+            length *= 2;
+            k++;
+            length += (k + 3);
+        }
+        System.out.println(moo(length, k, n));
+    }
+    private static char moo(int length, int k, int n){
+        int prevLen = (length - (k + 3)) / 2;
+        int midLen = prevLen + k + 3;
 
-	private static int target;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		target = Integer.parseInt(br.readLine());
-
-		int priorValue = 3;
-		int cnt = 1;
-		while (true) {
-			if (target < 2 * priorValue + cnt + 4) { // 길이 최소
-				break;
-			}
-			priorValue = 2 * priorValue + (cnt++) + 3;
-		}
-
-//		System.out.println(S); //10 => S(1) 길이 값
-//		System.out.println(cnt); //2 => S(2) 부터 탐색해야함
-
-		solve(priorValue, cnt);
-
-	}
-
-	private static void solve(int s, int cnt) { // 길이, 길이만큼 조회
-		// solve
-		int length = cnt + 3;
-		int prior = (s - length + 1) / 2;
-
-
-		// 기저조건
-		if (target >= s + 1 && target < s + 1 + length) {
-			if (s + 1 == target) { // 만약 target이 같으면
-				System.out.println("m");
-			} else {
-				System.out.println("o");
-			}
-			return;
-		}
-		else if (target <= s) { // 작을 경우
-			solve(prior, cnt - 1);
-		} 
-		else if (s + length < target) {
-			
-			target = target - (s + length);
-			solve(prior, cnt - 1);
-		}
-
-	}
-
+        if(k == 0){
+            if(n == 1){
+                return 'm';
+            }else{
+                return 'o';
+            }
+        }
+        if(n <= prevLen){
+            return moo(prevLen, k - 1, n);
+        } else if(n <= midLen){
+            if(n == prevLen + 1){
+                return 'm';
+            }else{
+                return 'o';
+            }
+        } else{
+            n -= midLen;
+            return moo(length - midLen, k - 1, n);
+        }
+    }
 }
