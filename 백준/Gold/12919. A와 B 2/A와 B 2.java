3a74a20c1874f01df49f03d920098968a4f46659
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-23 16:10:27 +0900

    [Gold V] Title: A와 B 2, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,41 +1,55 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static String S, T;
-    static boolean flag;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        S = br.readLine();
-        T = br.readLine();
 
-        dfs(T, T.length());
+	private static String t;
+	private static String sb;
+	static boolean flag = false;
 
-        System.out.println(flag ? 1 : 0);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = br.readLine();
+		t = br.readLine();
+		
+		solved(t);
+		System.out.println((flag)?1:0);
+		
+	}//end main
 
-    static void dfs(String str ,int depth){
-        if(depth == S.length()){
-            if(str.equals(S)){
-                flag = true;
-            }
-            return;
-        }
+	private static void solved(String t) { //t->s
+		StringBuilder answer = new StringBuilder();
+		answer.append(t);
+		
+		//기저조건
+		if(answer.toString().equals(sb.toString())) {
+			flag = true;
+			return;
+		}
+		
+		if(answer.length() <= sb.length()) {
+			return;
+		}
+		
+		//문자열 맨앞에 B가 있으면 B를 삭제할 수 있음 BAB -> BA, BA -> A
+		//맨앞에 B가 있으면 reverse() 후 맨뒤 B 삭제
+		if(answer.charAt(0) == 'B') {
+			StringBuilder sub = new StringBuilder();
+			sub.append(answer);
+			sub.reverse();
+			solved(sub.substring(0,sub.length()-1));
+		}
+		
+		//문자열의 맨 뒤에 A가 있으면 A를 삭제할 수 있음
+		if(answer.charAt(answer.length()-1) == 'A') {
+			StringBuilder sub = new StringBuilder();
+			sub.append(answer);
+			solved(sub.substring(0, sub.length()-1));
+		}
 
-        if(str.charAt(0) == 'B'){
-            dfs(reverse(str.substring(1)), depth - 1);
-        }
-        if (str.charAt(str.length() - 1) == 'A'){
-            dfs(str.substring(0, str.length() - 1), depth - 1);
-        }
-    }
+		
+		
+	}
 
-    static String reverse(String str){
-        char[] cStr = str.toCharArray();
-        char[] reverseStr = new char[cStr.length];
-        for (int i = 0; i < cStr.length; i++) {
-            reverseStr[cStr.length - 1 - i] = cStr[i];
-        }
-
-        return new String(reverseStr);
-    }
-}
+}//end class
