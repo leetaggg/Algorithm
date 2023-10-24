df8655d5e9cd82d430994cc29352a1aa0a051bde
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-10-24 17:20:10 +0900

    [Gold V] Title: 용액, Time: 372 ms, Memory: 30252 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,46 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-/*
--99 98
-* */
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 산성용액 : 1 ~ 1_000_000_000 양의 정수
+		// 알칼리성 : -1 ~ -1_000_000_000 음의 정수
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		// 특성 값이 0에 가장 가까운 용액을 만들려고 한다
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int[] arr = new int[n];
+		long[] arr = new long[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Long.parseLong(st.nextToken());
+		}
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+		int s = 0; // 인덱스 1
+		int e = n - 1; // 인덱스 2
+		long answer = Long.MAX_VALUE; // 최종 0과 가까운 값
+		int ai = 0; // 값이 될 것 담아두는 인덱스 1
+		int aj = 0; // 값이 될 것 담아두는 인덱스 2
+		long target = 0;
 
-        int left = 0;
-        int right = arr.length - 1;
-        int sum = 0;
+//		System.out.println(Arrays.toString(arr));
+		while (s<e) {
 
-        int resultLeft = 0;
-        int resultRight = 0;
-        int minValue = Integer.MAX_VALUE;
+			target = arr[s] + arr[e]; // 두 포인 터
 
-        while(left < right){
-            sum = arr[left] + arr[right];
-            if(Math.abs(minValue) > Math.abs(sum)){
-                minValue = sum;
-                resultLeft = left;
-                resultRight = right;
-            }
-            if(sum == 0){
-                break;
-            }else if(sum < 0){
-                left++;
-            }else{
-                right--;
-            }
-        }
-        System.out.println(arr[resultLeft] + " " + arr[resultRight]);
-    }
+			if (answer > Math.abs(target)) {
+				answer =  Math.abs(target); // answer 값 바꾸어줌
+				ai = s;
+				aj = e;
+			}
+			if(target >= 0) {
+				e--;
+			}else {
+				s++;
+			}
+			
+
+		}
+		System.out.println(arr[ai] + " " + arr[aj]);
+
+	}
+
 }
