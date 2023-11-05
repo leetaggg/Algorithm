e5d0c037ab7c839e40a60dc60c2fff7e54ff89aa
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-11-05 23:44:50 +0900

    [Gold V] Title: 단어 마방진, Time: 204 ms, Memory: 16592 KB -BaekjoonHub

--- 백준/Gold/24891. 단어 마방진/단어 마방진.java
+++ 백준/Gold/24891. 단어 마방진/단어 마방진.java
@@ -1,94 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int l, n;
+    static String[] words, result;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	private static String[] words, answer;
-	private static int r;
-	private static int c;
-	private static boolean[] isSelected;
-	static StringBuilder sb = new StringBuilder();
+        l = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        words = new String[n];
+        result = new String[l];
+        visited = new boolean[n];
 
-		c = Integer.parseInt(st.nextToken());
-		r = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+        }
 
-		words = new String[r];
-		answer = new String[c];
+        Arrays.sort(words);
 
-		for (int i = 0; i < r; i++) {
-			words[i] = br.readLine();
-		} // 입력 완료
+        StringBuilder sb = new StringBuilder();
 
-		Arrays.sort(words);
+        if(dfs(0)){
+            for (int i = 0; i < l; i++) {
+                for (int j = 0; j < l; j++) {
+                    sb.append(result[i].charAt(j));
+                }
+                sb.append("\n");
+            }
+        }else{
+            sb.append("NONE");
+        }
 
-		numbers = new int[c];
-		isSelected = new boolean[r];
+        System.out.println(sb);
 
-		solved(0);
-		if(sb.toString().equals("")) {
-			sb.append("NONE");
-		}
-		System.out.println(sb.toString());
+    }
 
-	}// end main
+    public static boolean dfs(int depth){
+        if(depth == l){
+            if(isPossible(result)){
+                return true;
+            }
+            return false;
+        }
 
-	static int[] numbers;
-	static boolean flag = false;
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                result[depth] = words[i];
+                if(dfs(depth + 1)){
+                    return true;
+                }
+                visited[i] = false;
+            }
+        }
+        return false;
+    }
 
-	public static void solved(int depth) { // 단어들 조합 콤비
-		if(flag) {
-			return;
-		}
-		// 기저조건
-		if (depth == c) {
-			for (int i = 0; i < c; i++) {
-				answer[i] = words[numbers[i]];
-			}
-			// 만들어진 String[] 을 갖고 마방진인지 check;
-			// check 에서 true 일 경우 String[] 차례로 출력
-			if(checked(answer)) {
-				for (int i = 0; i < c; i++) {
-					sb.append(answer[i]).append("\n");
-				}
-				flag = true;
-			}
-			return;
-		}
-
-		for (int i = 0; i < r; i++) {
-			if (isSelected[i])
-				continue;
-			numbers[depth] = i;
-			isSelected[i] = true;
-			solved(depth + 1);
-			isSelected[i] = false;
-		}
-
-	}
-
-	public static boolean checked(String[] temp) {
-		// 가로는 무조건 다 읽어야함.
-		// 가로 길이만큼 가로 세로 읽으면서 확인함.
-		// 틀렸을 경우
-
-		for (int i = 0; i < c; i++) {
-			for (int j = 0; j < c; j++) {
-				if(temp[j].charAt(i) != temp[i].charAt(j)) {
-					return false;
-				}
-				
-			}
-			
-		}
-		
-		return true;
-	}
-
-}// end class
+    public static boolean isPossible(String[] result){
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < l; j++) {
+                if (result[i].charAt(j) != result[j].charAt(i)) return false;
+            }
+        }
+        return true;
+    }
+}
