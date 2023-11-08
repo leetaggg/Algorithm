73fcf7d9a259d3c1cb7efbcbc134cbff38f29fc1
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-11-08 17:01:00 +0900

    [Gold IV] Title: 인문예술탐사주간, Time: 732 ms, Memory: 46052 KB -BaekjoonHub

--- 백준/Gold/23829. 인문예술탐사주간/인문예술탐사주간.java
+++ 백준/Gold/23829. 인문예술탐사주간/인문예술탐사주간.java
@@ -1,53 +1,74 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main{
 
-        int n = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
+	private static long[] arr;
+	private static int[] trees;
+	private static int n;
+	private static int q;
 
-        int[] arr = new int[n];
-        long[] prefix = new long[n + 1];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		q = Integer.parseInt(st.nextToken());
+		
+		trees = new int[n+1];
+		arr = new long[n+1];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 1; i <= n; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(trees);
+		for (int i = 1; i <= n; i++) {
+			arr[i] = arr[i-1]+trees[i];
+		}
+		for (int i = 0; i < q; i++) { 
+			long x = Integer.parseInt(br.readLine());//target
+			int start = solved(x);//이진탐색 start점 찾기(trees의 인덱스 점)
+			long first;
+			long second;
+			
+			if(trees[start] <= x) {
+				first = x*(start) - arr[start]; 
+				second = (arr[n] - arr[start]) - ((long)x*(n-start));
+			}else {
+				first = x*(start-1) - (arr[start-1]); 
+				second = (arr[n] - arr[start-1]) - ((long)x*(n-start+1));
+			}
+			long answer = first + second;
+			
+			sb.append(answer).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}//end main
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + arr[i];
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-
-        for (int i = 0; i < q; i++) {
-            int p = Integer.parseInt(br.readLine());
-            int idx = binarySearch(arr, p);
-
-            sb.append((prefix[n] - prefix[idx] - ((long) p * (n - idx))) + (Math.abs(prefix[idx] - ((long) p * idx)))).append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    static int binarySearch(int[] arr, int p){
-        int left = 0;
-        int right = arr.length;
-
-        while(left < right){
-            int mid = (left + right) / 2;
-
-            if(arr[mid] < p){
-                left = mid + 1;
-            }else{
-                right = mid;
-            }
-        }
-        return right;
-    }
-}
+	private static int solved(long target) {
+		int l = 1;
+		int r = n;
+		int mid = 0;
+		
+		while(l <= r) {
+			mid = (l+r)/2;
+			
+			if(trees[mid] < target) {
+				l =  mid + 1;
+			}else if(trees[mid] > target){
+				r = mid - 1;
+			}else {
+				break;
+			}
+		}
+		
+		return mid;
+		
+	}
+	
+}//end class
