fb68db44f789beb89c70c5344cb5cc052009559b
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-11-15 08:40:04 +0900

    [Silver III] Title: N과 M (5), Time: 336 ms, Memory: 29472 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,53 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n, m;
-    static int[] numbers, inputs;
-    static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
+	private static int[] arr;
+	private static int n;
+	private static int m;
+	private static boolean[] isSelected;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken()); // 입력 수
+		m = Integer.parseInt(st.nextToken()); // 뽑을 개수
+		
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}// 입력 완료
+		Arrays.sort(arr);
+		
+		numbers = new int[m]; //수열을 담을 배열
+		isSelected = new boolean[n]; //중복을 막을 배열
+		
+		per(0);
+		System.out.println(sb.toString());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	}//end main
+	
 
-        inputs = new int[n];
-        numbers = new int[m];
-        visited = new boolean[n];
+	public static void per(int cnt) {
+		//기저조건
+		if(cnt == m) {
+			print(numbers);
+			return;
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(!isSelected[i]) {
+				numbers[cnt] = arr[i];
+				isSelected[i] = true;
+				per(cnt+1);
+				isSelected[i] = false;
+			}
+			
+		}
+	}//end per
+	
+	public static void print(int[] numbers) {
+		
+		for (int i : numbers) {
+			sb.append(i).append(" ");
+		}
+		sb.append("\n");
+	}// end print
 
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            inputs[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(inputs);
-
-        permutation(0);
-
-        System.out.println(sb);
-    }
-
-    public static void permutation(int cnt){
-        if(cnt == m){
-            for (int i : numbers) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                numbers[cnt] = inputs[i];
-                permutation(cnt + 1);
-                visited[i] = false;
-            }
-        }
-    }
-}
+}//end class
