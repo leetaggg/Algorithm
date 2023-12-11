5deb04adc1735a40fd48bc87e2f04a3d73a1dc2d
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-12-12 02:38:20 +0900

    [Silver V] Title: 숫자 카드, Time: 700 ms, Memory: 152856 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -7,14 +7,12 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] cards = new int[n];
+        boolean[] cards = new boolean[20000001];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            cards[10000000 + Integer.parseInt(st.nextToken())] = true;
         }
-
-        Arrays.sort(cards);
 
         int m = Integer.parseInt(br.readLine());
 
@@ -22,27 +20,9 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            sb.append(binarySearch(cards , Integer.parseInt(st.nextToken()))).append("\n");
+            sb.append(cards[10000000 + Integer.parseInt(st.nextToken())] ? 1 : 0).append("\n");
         }
 
         System.out.println(sb);
-    }
-
-    static int binarySearch(int[] cards, int target){
-        int left = 0;
-        int right = cards.length - 1;
-
-        while(left <= right){
-            int mid = (left + right) / 2;
-
-            if(target < cards[mid]){
-                right = mid - 1;
-            }else if(target > cards[mid]){
-                left = mid + 1;
-            }else{
-                return 1;
-            }
-        }
-        return 0;
     }
 }
