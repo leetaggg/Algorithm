769b26f02f02b1a4d393ebc94f781da5016b70b1
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2023-12-23 21:07:38 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,28 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		
-		for (int i = n; i >= 0; i--) {
-			// 별찍기
-			for (int j = 0; j < i ; j++) {
-				sb.append("*");
-			}
-			//빈칸 찍기
-			for (int j = 0; j < n-i; j++) {
-				sb.append("");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb.toString());
-		
-	}
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
