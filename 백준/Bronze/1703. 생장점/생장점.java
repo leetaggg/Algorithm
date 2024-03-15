9c91f28595d05304bb266431e761cb86f987ba3d
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2024-03-15 22:22:44 +0900

    [Bronze III] Title: 생장점, Time: 76 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/1703. 생장점/생장점.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int tree = 1;
+
+            if(n == 0) {
+                break;
+            }
+
+            for(int i = 0; i < 2 * n; i++) {
+                int m = Integer.parseInt(st.nextToken());
+                if(i % 2 == 0) {
+                    tree *= m;
+                }else {
+                    tree -= m;
+                }
+            }
+            System.out.println(tree);
+        }
+    }
+
+}
