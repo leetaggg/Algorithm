44e1ba2ef50f541bd4041c6f90a34753d424a0d3
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2024-05-19 23:50:30 +0900

    [Bronze V] Title: 긴자리 계산, Time: 136 ms, Memory: 13512 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -0,0 +1,15 @@
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger first = in.nextBigInteger();
+        BigInteger second = in.nextBigInteger();
+
+        System.out.println(first.add(second));
+        System.out.println(first.subtract(second));
+        System.out.print(first.multiply(second));
+
+    }
+}
