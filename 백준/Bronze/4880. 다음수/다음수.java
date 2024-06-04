ac4b76e0b1c94ef0046bf6b6e1fc0904ff0bdb8b
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2024-06-05 00:17:31 +0900

    [Bronze III] Title: 다음수, Time: 116 ms, Memory: 12512 KB -BaekjoonHub

---
+++ 백준/Bronze/4880. 다음수/다음수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			if(a == 0 && b == 0 && c == 0) {
+				break;
+			}
+
+			int n = b - a;
+			int m = c - b;
+
+			String result = (n == m) ? ("AP " + (c + m)) : ("GP " + (c + (m * (m / n))));
+
+			System.out.println(result);
+		}
+	}
+}
