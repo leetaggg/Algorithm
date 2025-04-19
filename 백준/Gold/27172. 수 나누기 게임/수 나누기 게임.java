ed49a2af423a3025903d59c63f7ae5a0749126d1
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2025-04-19 16:45:15 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 296 ms, Memory: 35304 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -2,43 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
+        int[] cards = new int[n];
+        boolean[] exists = new boolean[1_000_001];
+        int max = 0;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+            exists[cards[i]] = true;
+            max = Math.max(max, cards[i]);
+        }
 
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] score = new int[1_000_001];
 
-		int[] cards = new int[n];
-		boolean[] isPrime = new boolean[1_000_001];
-		int max = 0;
+        for (int i = 1; i <= max; i++) {
+            if (!exists[i]) continue;
 
-		for (int i = 0; i < n; i++) {
-			cards[i] = Integer.parseInt(st.nextToken());
-			max = Math.max(max, cards[i]);
-			isPrime[cards[i]] = true;
-		}
+            for (int j = i * 2; j <= max; j += i) {
+                if (exists[j]) {
+                    score[i]++;
+                    score[j]--;
+                }
+            }
+        }
 
-		int[] arr = new int[max + 1];
-
-		for (int i = 1; i <= max; i++) {
-			if (!isPrime[i]) continue;
-
-			for (int j = i * 2; j <= max; j += i) {
-				if (isPrime[j]) {
-					arr[i]++;
-					arr[j]--;
-				}
-			}
-		}
-
-
-		StringBuilder sb = new StringBuilder();
-		for (int i : cards) {
-			sb.append(arr[i]).append(" ");
-		}
-
-		System.out.println(sb);
-	}
+        StringBuilder sb = new StringBuilder();
+        for (int card : cards) {
+            sb.append(score[card]).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
