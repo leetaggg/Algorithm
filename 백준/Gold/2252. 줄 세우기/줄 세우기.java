787cd6150ad6ec39c617a2edd819ab926af38951
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2025-08-19 20:04:28 +0900

    [Gold III] Title: 줄 세우기, Time: 384 ms, Memory: 50496 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,65 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken()); //키를 비교한 회수
-		
-		List<Integer>[] list = new ArrayList[n+1]; 
-		for (int i = 0; i < list.length; i++) {
-			list[i] = new ArrayList<Integer>();
-		}
-		
-		int[] isEdge = new int[n+1];
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			list[a].add(b); //그래프 저장
-			isEdge[b] += 1; //간선 수 저장
-			
-		}
-		//위상정렬 시작
-		Queue<Integer> queue = new ArrayDeque<Integer>();
-		
-		for(int i = 1; i <= n; i++) {
-			if(isEdge[i] == 0) { //0일경우 큐에 넣기
-				queue.offer(i);//0인값 큐에 넣기
-			}
-		}
-		
-		while(!queue.isEmpty()) {
-			int current = queue.poll(); //0인 정점과 연결된 것 찾기
-			sb.append(current).append(" ");
-			
-			for (int i = 0; i < list[current].size(); i++) {
-				int next = list[current].get(i); // 인접 정점
-				isEdge[next] -= 1;
-				
-				if(isEdge[next] == 0) { //뺀 값 0 이 되면 offer
-					queue.offer(next);
-				}
-				
-			}
-			
-		}
-		
-		System.out.println(sb.toString());
-		
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] indegree = new int[n + 1];
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph.get(from).add(to);
+            indegree[to]++;
+        }
+
+        topologicalSort(graph, indegree, n);
+
+        System.out.println(sb);
+    }
+
+    static void topologicalSort(List<List<Integer>> graph, int[] indegree, int n) {
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int node =  q.poll();
+
+            sb.append(node + " ");
+
+            for (int i : graph.get(node)) {
+                indegree[i]--;
+
+                if (indegree[i] == 0) {
+                    q.add(i);
+                }
+            }
+        }
+    }
 }
