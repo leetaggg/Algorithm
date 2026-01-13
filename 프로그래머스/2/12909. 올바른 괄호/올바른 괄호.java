38c8b55e7d693c2d642007a5c870b3de1a8aa9e7
LeeTaeHo <100212241+leetaggg@users.noreply.github.com>
2026-01-13 20:23:14 +0900

    [level 2] Title: 올바른 괄호, Time: 17.59 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
